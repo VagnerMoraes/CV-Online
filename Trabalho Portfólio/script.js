9112309c2346db413d1c683bf736d5e82a346a5d
VagnerMoraes <53999562+VagnerMoraes@users.noreply.github.com>
2022-08-04 21:57:58 -0300

    Trabalho Portfólio
    Arquivos do Trabalho Finalizado

---
+++ Trabalho Portfólio/script.js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+    var nome = document.getElementById("emailid");
+
+    if (nome.value != "") {
+        alert('Obrigado a sua solicitação foi encaminhada com sucesso');
+    }
+
+}
